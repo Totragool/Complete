112674315789360f204cc524eed5166b98ed5d45
Totragool <totragool24@gmail.com>
2025-01-16 13:32:24 +0700

    a

--- backend/main.go
+++ backend/main.go
@@ -83,7 +83,7 @@ func main() {
 
     // Setup CORS
     r.Use(cors.New(cors.Config{
-        AllowOrigins:     []string{"http://localhost:5174", "http://localhost:3000"},
+        AllowOrigins:     []string{"http://localhost:5173", "http://localhost:3000"},
         AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
         AllowHeaders:     []string{"Origin", "Content-Type", "Accept", "Authorization"},
         ExposeHeaders:    []string{"Content-Length"},
